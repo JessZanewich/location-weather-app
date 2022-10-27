226e030c795d65af70b966fcf33d0b8447b51dd8
Jess von Euw <jessvoneuw@Jesss-MacBook-Pro.local>
2022-10-27 16:39:22 -0600

    add initial data fetch and display on browser

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,56 @@
 import './App.css';
+import { useEffect, useState } from "react";
 
-function App() {
+interface Weather {
+  main: any,
+  weather: Array<any>,
+  name: string,
+  coord: any
+}
+
+const App = () => {
+  const [city, setCity] = useState('calgary');
+  const [data, setData] = useState<Weather[]>();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      await fetch(`${process.env.REACT_APP_WEATHER_URL}/weather?q=${city}&units=metric&appid=${process.env.REACT_APP_WEATHER_API_KEY}`)
+        .then(res => res.json())
+        .then(result => {
+          console.log(result);
+        });
+    }
+    fetchData();
+  }, [city]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      await fetch(`${process.env.REACT_APP_WEATHER_URL}/forecast?q=${city}&units=metric&appid=${process.env.REACT_APP_WEATHER_API_KEY}`)
+        .then(res => res.json())
+        .then(result => {
+          const dailyForecast = result.list.filter((forecast: any) => { console.log(forecast.dt_txt.includes('12:00:00')); return forecast.dt_txt.includes('12:00:00'); });
+          setData(dailyForecast);
+        });
+    }
+    fetchData();
+  }, [city]);
+
   return (
     <div className="App">
-
+      <div>
+        <div onClick={() => setCity('ottawa')}>Ottawa</div>
+        <div onClick={() => setCity('moscow')}>Moscow</div>
+        <div onClick={() => setCity('tokyo')}>Tokyo</div>
+      </div>
+      {data && (
+        <div>
+          <div>Today</div>
+          <img src={`${process.env.REACT_APP_WEATHER_ICON}/03d.png`} />
+          {data.length !== 0 && data.map((weather) =>
+            <div>{Math.round(weather.main.temp)}</div>
+          )}
+        </div>
+      )}
     </div>
   );
 }
